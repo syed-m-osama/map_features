cb877d7fd64f0a4b66a0ade299c8e1827f72e212
Osama <syed.m.osama.new@gmail.com>
2022-04-08 02:32:26 +0530

    playback functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:map_features/screens/geo_fencing.dart';
 import 'screens/playback.dart';
-import 'screens/realtime_tracker.dart';
-import 'screens/geo_fencing.dart';
 import 'screens/marker_cluster..dart';
 import 'screens/route_mapping.dart';
-import 'screens/map_icon_change.dart';
-import 'screens/realtime_loc.dart';
-import 'screens/loc_customised_data.dart';
+import 'screens/realtime_location/realtime_loc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,12 +23,9 @@ class MyApp extends StatelessWidget {
       routes: {
         "realtime_loc": (context) => const RealtimeLocCapSend(),
         "route_mapping": (context) => const RouteMapping(),
-        "map_icon_change": (context) => const MapIconChange(),
-        "loc_customised_data": (context) => const LocCustomisedData(),
         "marker_cluster": (context) => const MarkerCluster(),
-        "geo_fencing": (context) => const GeoFencing(),
-        "realtime_tracker": (context) => const RealtimeTracker(),
-        "playback":(context) => const Playback()
+        "geo_fencing": (context) => GeoFencing(),
+        "playback": (context) => const Playback()
       },
     );
   }
@@ -52,15 +46,11 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            RouteButton(title: 'Realtime Loc Cap Send', routeName: 'realtime_loc'),
-            RouteButton(title: 'Route Mapping', routeName: 'route_mapping'),
-            RouteButton(title: 'Map Icon Change', routeName: 'map_icon_change'),
-            RouteButton(title: 'Loc Customised Data', routeName: 'loc_customised_data'),
+            RouteButton(title: 'Realtime Location', routeName: 'realtime_loc'),
             RouteButton(title: 'Marker Cluster', routeName: 'marker_cluster'),
             RouteButton(title: 'Geo-Fencing', routeName: 'geo_fencing'),
-            RouteButton(title: 'Realtime Tracker', routeName: 'realtime_tracker'),
-            RouteButton(title: 'PlayBack', routeName: 'playback')
-
+            RouteButton(title: 'Playback', routeName: 'playback'),
+            RouteButton(title: 'Route Mapping', routeName: 'route_mapping')
           ],
         ),
       ),
@@ -68,9 +58,9 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-
 class RouteButton extends StatelessWidget {
-  const RouteButton({ Key? key ,required this.title, required this.routeName}) : super(key: key);
+  const RouteButton({Key? key, required this.title, required this.routeName})
+      : super(key: key);
 
   final String title;
   final String routeName;
@@ -78,9 +68,9 @@ class RouteButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: () => Navigator.pushNamed(context, routeName),
-      child: Text(title),
-      textColor: Colors.white,
-      color: Colors.green[600]);
+        onPressed: () => Navigator.pushNamed(context, routeName),
+        child: Text(title),
+        textColor: Colors.white,
+        color: Colors.green[600]);
   }
 }
--- lib/screens/playback.dart
+++ lib/screens/playback.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:developer';
+import 'package:flutter_animarker/flutter_map_marker_animation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Playback extends StatefulWidget {
-  const Playback({ Key? key }) : super(key: key);
+  const Playback({Key? key}) : super(key: key);
 
   @override
   State<Playback> createState() => _PlaybackState();
@@ -10,9 +14,82 @@ class Playback extends StatefulWidget {
 class _PlaybackState extends State<Playback> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Playback")),
-      body: const Center(child: Text('Empty')),
+    return MarkerAnimation();
+  }
+}
+
+//playback
+//Setting dummies values
+const kStartPosition = LatLng(28.688058, 77.082642);
+const kDelhi = CameraPosition(target: kStartPosition, zoom: 15);
+const kMarkerId = MarkerId('MarkerId1');
+const kDuration = Duration(seconds: 2);
+const kLocations = [
+  kStartPosition,
+  LatLng(28.688058, 77.083479),
+  LatLng(28.688049, 77.084670),
+  LatLng(28.688030, 77.086676),
+  LatLng(28.687983, 77.087566),
+  LatLng(28.688002, 77.089047),
+  LatLng(28.687946, 77.090452),
+  LatLng(28.687946, 77.091396),
+  LatLng(28.687937, 77.092340),
+  LatLng(28.687871, 77.093488),
+  LatLng(28.688059, 77.094539),
+  LatLng(28.688360, 77.095354),
+  LatLng(28.689141, 77.095365),
+  LatLng(28.688830, 77.095955),
+  LatLng(28.689734, 77.096298),
+  LatLng(28.691748, 77.097888),
+  LatLng(28.693094, 77.100302),
+  kStartPosition
+];
+
+class MarkerAnimation extends StatefulWidget {
+  @override
+  MarkerAnimationState createState() => MarkerAnimationState();
+}
+
+class MarkerAnimationState extends State<MarkerAnimation> {
+  final markers = <MarkerId, Marker>{};
+  final controller = Completer<GoogleMapController>();
+  final stream = Stream.periodic(kDuration, (count) => kLocations[count])
+      .take(kLocations.length);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Animarker(
+        rippleRadius: 0,
+        useRotation: true,
+        duration: Duration(seconds: 2),
+        mapId: controller.future
+            .then<int>((value) => value.mapId), //Grab Google Map Id
+        markers: markers.values.toSet(),
+        child: GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: kDelhi,
+            onMapCreated: (gController) {
+              stream.forEach((value) => newLocationUpdate(value));
+              controller.complete(gController);
+              //Complete the future GoogleMapController
+            }),
+      ),
     );
+  }
+
+  void newLocationUpdate(LatLng latLng) {
+    var marker = RippleMarker(
+        markerId: kMarkerId,
+        position: latLng,
+        onTap: () {
+          log('Tapped! $latLng');
+        });
+    setState(() => markers[kMarkerId] = marker);
   }
 }
